506a19ae12f5636121900766ceb4433a6aeb71ba
xlsn0w <xlsn0w@outlook.com>
2017-12-06 15:14:50 +0800

    init

---
+++ XLsn0wAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XLsn0wAnimation
+//
+//  Created by golong on 2017/12/6.
+//  Copyright © 2017年 XLsn0w. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
