a85e09ba4e916051e76b7ebb9c3c25b7000f087b
XLsn0w <XLsn0w@outlook.com>
2018-09-03 22:47:48 +0800

    add

---
+++ XLsn0wAnimation/objc-js-interact/WebViewDelegate/WebViewDelegateViewController.m
@@ -0,0 +1,37 @@
+//
+//  WebViewDelegateViewController.m
+//  XLsn0wAnimation
+//
+//  Created by XLsn0w on 2018/9/3.
+//  Copyright © 2018年 XLsn0w. All rights reserved.
+//
+
+#import "WebViewDelegateViewController.h"
+
+@interface WebViewDelegateViewController ()
+
+@end
+
+@implementation WebViewDelegateViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
